c77afff35d60133806cbe7707056efc61acf2f71
Janitha Silva <Janitha.Silva@dialog.lk>
2021-02-18 18:06:25 +0530

    initial commit

--- src/scaffold/project/main.ts
+++ src/scaffold/project/main.ts
@@ -1,5 +1,6 @@
-import { platformBrowser } from '@angular/platform-browser';
-import { enableProdMode } from '@angular/core';
-import { AppModuleNgFactory } from './ngfactory/src/app/app.module.ngfactory';
+import {platformBrowser} from '@angular/platform-browser';
+import {enableProdMode} from '@angular/core';
+import {AppModuleNgFactory} from './ngfactory/src/app/app.module.ngfactory';
+
 enableProdMode();
 platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
